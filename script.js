2a23ee48eaba968d9330c71fbf9f3c43c9353956
Arsal Jan <arsaljaan532@gmail.com>
2025-09-19 10:30:38 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
- var birthYear = +prompt("Enter your birth year:");
-    var currentYear = new Date().getFullYear();
-    var age = currentYear - birthYear;
-
-    if (birthYear > currentYear || birthYear <= 0) {
-      alert("Invalid birth year entered!");
-    } else {
-      alert("Your age is: " + age + " years");
-    }
